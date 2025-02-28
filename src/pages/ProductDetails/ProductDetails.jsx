c9c0dc85d8fec621861626cb38273348ac7ba6f7
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2025-02-28 09:15:12 +0200

    add lv in product details

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -160,8 +160,8 @@ const ProductDetails = () => {
 
             <div className="product-details">
             <h2>{product.productname}</h2>
-                <p className="old-price">Стара Цена: {product.price}</p>
-                <p className="product-price">Цена: {product.discount_price}</p>
+                <p className="old-price">Стара Цена: {product.price}лв.</p>
+                <p className="product-price">Цена: {product.discount_price}лв.</p>
 
                 <label className='product-quantity-input-label'>
                     <input className="product-quantity-input"
